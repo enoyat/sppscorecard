a00ff39dfcc69d6f1768a4da34b62ef692f6926f
enoyat <50427196+enoyat@users.noreply.github.com>
2023-06-17 23:35:31 +0700

    1

---
+++ Admin/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
+
+//Update User Details
+Route::post('/update-profile/{id}', [App\Http\Controllers\HomeController::class, 'updateProfile'])->name('updateProfile');
+Route::post('/update-password/{id}', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('updatePassword');
+
+Route::get('{any}', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+
+//Language Translation
+Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang']);
+
